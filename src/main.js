4d8c3487f660bb4a04c9cc84a1446e3110dc0a9a
Max Adulyanukosol <namax7@gmail.com>
2019-02-08 13:04:43 +0700

    Optimize for mobile screen

--- src/main.js
+++ src/main.js
@@ -559,7 +559,8 @@ function drawResultConstituent(
     .select(selector)
     .attr('width', width)
     .attr('height', height)
-    .style('overflow', 'visible');
+    .style('overflow', 'visible')
+    .style('flex', `0 0 ${width}px`);
 
   svg.selectAll('*').remove();
 
@@ -1085,7 +1086,9 @@ function addFinalAllocationText(electionResult, electionConfig, selector) {
     พรรคนี้ได้รับส.ส.ตามที่ได้มาจากแบบแบ่งเขตเลือกตั้ง ● แต่ไม่สามารถมีส.ส.แบบบัญชีรายชื่อ ◆ ได้อีก</p>
     <p>ดังนั้นการจัดสรรส.ส.พึงมีและส.ส.บัญชีรายชื่อจึงพิจารณาจาก ${electionConfig.nParty -
       electionResult.nPartyWithoutPartyListNeeded} พรรคที่เหลือเพียงเท่านั้น
-      โดยใช้เสียง ${numberWithCommas(electionResult.nVotePerRemainingSeat)} ต่อ 1 ที่นั่ง</p>`;
+      โดยใช้เสียง ${numberWithCommas(
+        electionResult.nVotePerRemainingSeat
+      )} ต่อ 1 ที่นั่ง</p>`;
   } else {
     text = `<p>เนื่องจากไม่มีพรรคใด ได้รับจำนวนส.ส.แบบเบ่งเขต ● ไปครบจำนวนส.ส.พึงมี ■</p>
     <p>ดังนั้นทุกพรรคจะได้รับจำนวนส.ส.แบบบัญชีรายชื่อ ◆ รวมกับส.ส.แบบเบ่งเขต ● จนครบจำนวนส.ส.พึงมี ■</p>`;
